0583ce9de8474bf293c7e693b1c0b1b5464ed2f9
Anıl Han Aydın <anilhanaydin4@gmail.com>
2026-05-03 00:53:52 +0300

    Final Commit

--- get_pizzas.php
+++ get_pizzas.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
@@ -18,14 +17,4 @@ try {
 } catch(PDOException $e) {
     echo json_encode(["error" => $e->getMessage()]);
 }
-=======
-header("Content-Type: application/json");
-include 'config.php';
-
-$stmt = $conn->prepare("SELECT * FROM pizzas");
-$stmt->execute();
-$pizzas = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($pizzas);
->>>>>>> 7c451b73f36acddd079fcfe17295f92de09687d5
 ?>
